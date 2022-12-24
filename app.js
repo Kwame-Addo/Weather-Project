ea32affba39d0ca1a7e9de831f1e740964e81c7d
Kwame Okai Addo <48158461+Kwame-Addo@users.noreply.github.com>
2022-12-24 22:03:48 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function(req,res){
+app.use(bodyParser.urlencoded({extended: true}));
 
-const query = "Accra";
+app.get("/", function(req,res){
+res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", function(req,res){
+const query = req.body.CityName;
 const apiKey = "aab1dfe1303685e1900d0e4db136d0e3";
 const unit = "metric";
 const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid="+ apiKey +"&units="+ unit;
@@ -21,13 +27,14 @@ https.get(url, function(response){
   const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
 
   res.write("<p>The weather is currently " + description + "<P>");
-  res.write("<h1>The Temperature in Accra is " + temp + " degrees Celcius</h1>");
+  res.write("<h1>The Temperature in "+ query + " is " + temp + " degrees Celcius</h1>");
   res.write("<img src=" + imageURL + ">");
   res.send()
   })
 })
 
 })
+
 
 app.listen(3000, function(){
   console.log("Server is running on port 3000.")
